544e91db64847b296572447d9fefcf2693453cc7
parth <parth280197@gmail.com>
2019-08-28 00:58:13 -0500

    Healper Class Added(UserManager.cs)

---
+++ Project-Management/Helpers/UserManagement.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Project_Management.Models;
+
+namespace Project_Management.Helpers
+{
+  public class UserManagement
+  {
+    ApplicationDbContext db;
+    UserManager<User> userManager;
+    public UserManagement(ApplicationDbContext db)
+    {
+      this.db = db;
+      userManager = new UserManager<User>(new UserStore<User>(db));
+    }
+    public bool AddUserToRole(string UserId, string RoleName)
+    {
+      return userManager.AddToRole(UserId, RoleName).Succeeded;
+    }
+  }
+}
