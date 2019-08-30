0648a5ba0a072524dd073aa15ca0c1ac35dd5881
parth <parth280197@gmail.com>
2019-08-30 17:48:46 -0500

    Create method added in ProjectController

---
+++ Project-Management/Controllers/ProjectsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNet.Identity;
+using Project_Management.Helpers;
+using Project_Management.Models;
+using System.Web.Mvc;
+
+namespace Project_Management.Controllers
+{
+  public class ProjectsController : Controller
+  {
+    private ProjectManagement projectManagement;
+    private ApplicationDbContext db;
+    public ProjectsController()
+    {
+      db = new ApplicationDbContext();
+      projectManagement = new ProjectManagement(db);
+    }
+    // GET: Projects
+    [Authorize(Roles = "ProjectManager")]
+    [HttpGet]
+    public ActionResult Create()
+    {
+      return View();
+    }
+    [HttpPost]
+    public ActionResult Create(Project project)
+    {
+      if (ModelState.IsValid)
+      {
+        project.CompletedPercentage = 0;
+        project.CreatedBy = db.Users.Find(User.Identity.GetUserId());
+        projectManagement.CreateProject(project);
+      }
+      else
+      {
+        return HttpNotFound();
+      }
+      return View();
+    }
+  }
+}
---
+++ Project-Management/Helpers/ProjectManagement.cs
@@ -0,0 +1,24 @@
+﻿using Project_Management.Models;
+
+namespace Project_Management.Helpers
+{
+
+  public class ProjectManagement
+  {
+    private ApplicationDbContext dbContext;
+    public ProjectManagement(ApplicationDbContext dbContext)
+    {
+      this.dbContext = dbContext;
+    }
+    public bool CreateProject(Project project)
+    {
+      if (project != null)
+      {
+        dbContext.Projects.Add(project);
+        dbContext.SaveChanges();
+        return true;
+      }
+      return false;
+    }
+  }
+}
